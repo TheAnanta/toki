06d6786d4016045943034f35a00b69ffd0e0f10d
Manas Malla <manasmalla.dev@gmail.com>
2024-02-02 17:24:33 +0530

    added size utils
    Signed-off-by: Manas Malla <manasmalla.dev@gmail.com>

---
+++ lib/presentation/responsiveness.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class Responsiveness {
+  static late BuildContext context;
+  init(BuildContext rContext) {
+    context = rContext;
+  }
+}
+
+bool isSmallScreen() {
+  return MediaQuery.of(Responsiveness.context).size.width < 600;
+}
+
+bool isMediumScreen() {
+  return MediaQuery.of(Responsiveness.context).size.width >= 600 &&
+      MediaQuery.of(Responsiveness.context).size.width < 840;
+}
+
+bool isLargeScreen() {
+  return MediaQuery.of(Responsiveness.context).size.width >= 840;
+}
+
+bool isExtraLargeScreen() {
+  return MediaQuery.of(Responsiveness.context).size.width >= 1200;
+}
+
+double getResponsiveWidth(double width) {
+  return (MediaQuery.of(Responsiveness.context).size.width * width) / 390;
+}
+
+double getResponsiveHeight(double height) {
+  return (MediaQuery.of(Responsiveness.context).size.height * height) / 844;
+}
